3d1e4c614162dbfe4d5abb1a856e37c1f06ca93a
phylis13 <phylisnjeri54@gmail.com>
2023-02-23 19:08:12 +0300

    done

--- spec/01_create_students_spec.rb
+++ spec/01_create_students_spec.rb
@@ -1,16 +1,7 @@
-require_relative '../db/migrate/01_create_students'
-
-describe CreateStudents do
-  before do
-    sql = "DROP TABLE IF EXISTS students"
-    ActiveRecord::Base.connection.execute(sql)
-    
-    # Manually runs the migration
-    CreateStudents.new.change
-  end
-
-  it 'has a name' do
-    student = Student.create(name: "Steven")
-    expect(Student.where(name: "Steven").first).to eq(student)
+class CreateStudents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
   end
 end
